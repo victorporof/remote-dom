3aba1bb9b8869095ae61c866778ddb9413eca116
Victor <victor.porof@gmail.com>
2020-05-29 19:07:24 +0200

    Minor changes to example config to make it more generic
    Signed-off-by: Victor <victor.porof@gmail.com>

--- config.example.js
+++ config.example.js
@@ -1,14 +1,12 @@
-/*
-Copy this to config.js and replace with you environment specific settings
-*/
+/**
+ * Copy this to config.js and replace with you environment specific settings
+ */
 const config = {
   puppeteer: {},
 };
 
+config.puppeteer.executablePath = process.env.EXECUTABLE_PATH || "path/to/firefox";
 config.puppeteer.args = ["--jsconsole"];
 config.puppeteer.dumpio = true;
-config.puppeteer.executablePath =
-  process.env.EXECUTABLE_PATH ||
-  "/Users/bgrins/Code/mozilla-central/objdir.noindex/dist/Nightly.app/Contents/MacOS/firefox";
 
-module.exports = config;
+export default config;

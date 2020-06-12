1d2245300a8de67303a7db83e21370cea549556d
Victor <victor.porof@gmail.com>
2020-06-12 15:45:00 +0200

    Add babel for testing in older browsers
    Signed-off-by: Victor <victor.porof@gmail.com>

---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+};
